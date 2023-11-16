33569e1a006de664cd9e0a5a5ee5985f5d21d1bf
Lawson Allick <lallick@gmail.com>
2023-11-16 14:39:55 -0500

    Add html elements, javascript, and css to get mobile menu to fade in

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,22 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
-  header.classList.toggle("open");
+  if (header.classList.contains("open")) {
+    //close hamburger menu
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    //open hamburger menu
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
 });
